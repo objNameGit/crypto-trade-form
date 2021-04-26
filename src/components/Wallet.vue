b3e125ea4334874ebeb2a4159e29190bc44833f6
jackson <jackson.87@mail.ru>
2021-04-27 00:15:42 +0200

    modify wallet

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -3,17 +3,20 @@
     <div class="wallet__icon">
       <v-icon middle>mdi-wallet-outline</v-icon>
     </div>
-    <div class="wallet__currency-list currency-list">
+    <div class="wallet__currency-list currency-list currency-list_table">
       <div
         v-for="(currency, index) in wallet"
         :key="index"
-        class="currency-list__currency currency"
+        class="currency-list__currency currency currency-list_row"
       >
-        <span class="currency__value">
+        <span
+          class="currency__value currency-list_cell darken-3"
+          :class="{ 'warning-color': isNegativeValue(currency.quantity) }"
+        >
           {{ currency.quantity }}
         </span>
         <span
-          class="currency__name"
+          class="currency__name currency-list_cell"
           :style="{ color: getColor(currency.currencyName) }"
         >
           {{ currency.currencyName }}
@@ -25,29 +28,54 @@
 
 <script>
 import { mapGetters } from "vuex";
-
+import { calculateUtils } from "@mixins/calculateUtils.js";
 import { helper } from "@mixins/helper.js";
 
 export default {
+  name: "Wallet",
   computed: {
     ...mapGetters(["wallet", "currencyColor"]),
   },
-  mixins: [helper],
+  mixins: [calculateUtils, helper],
 };
 </script>
 
 <style scoped>
+.currency-list .currency:last-of-type {
+  margin-bottom: 0;
+}
+
 .wallet {
   display: flex;
   align-items: center;
-  margin-bottom: 15px;
+  margin-bottom: 16px;
 }
 
 .wallet__icon {
-  margin-right: 15px;
+  margin-right: 4px;
 }
 
 .currency__value {
-  margin-right: 5px;
+  margin-right: 4px;
+  font-size: 1.5rem;
+  font-weight: 500;
+  text-align: end;
+}
+
+.currency-list_table {
+  display: table;
+  border-spacing: 8px;
+}
+
+.currency-list_row {
+  display: table-row;
+}
+
+.currency-list_cell {
+  display: table-cell;
+}
+
+.warning-color {
+  color: crimson;
 }
 </style>
